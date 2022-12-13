b94bc240b7901d8a3f1187dc73e48d2516dcb547
B00mMaster <pablogaudesi@gmail.com>
2022-12-13 22:47:46 +0100

    Initial commit

---
+++ Assets/Scripts/Exercise1.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise1 : MonoBehaviour
+{
+    public int cash;
+
+    private int total;
+
+    private int newTotal;
+
+   /* private void Start()
+    {
+        MoneyBox(total, cash, newTotal);
+
+        NewBox();
+    
+    }
+
+
+    private void MoneyBox(int x,int y, int z)
+    {
+        if (x+(x+y)==z);
+
+        Debug.Log($"Ha depositado {x} euros. Tiene un total de {z}euros");
+
+
+        else if (x+(x+y)==z);
+        
+    
+    
+    }
+
+    private void NewBox()
+    {
+
+    }
+
+    */
+
+}
